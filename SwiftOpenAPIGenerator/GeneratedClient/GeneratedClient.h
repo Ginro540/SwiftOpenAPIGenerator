d5f2de00f71ecd726228fc6c64481cb510295c67
古賀貴伍 <jpx02428@linecorp.com>
2023-09-12 22:43:22 +0900

    初回コミット

---
+++ SwiftOpenAPIGenerator/GeneratedClient/GeneratedClient.h
@@ -0,0 +1,18 @@
+//
+//  GeneratedClient.h
+//  GeneratedClient
+//
+//  Created by 古賀貴伍社用 on 2023/09/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeneratedClient.
+FOUNDATION_EXPORT double GeneratedClientVersionNumber;
+
+//! Project version string for GeneratedClient.
+FOUNDATION_EXPORT const unsigned char GeneratedClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeneratedClient/PublicHeader.h>
+
+
